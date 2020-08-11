43d21b936b7395afb5f6e9565bb7f2159621736d
Michael Thaler <michael.thaler@dlr.de>
2020-08-11 13:31:27 +0200

    Removed getString function, which is no longer needed.

--- http/utils.go
+++ http/utils.go
@@ -1,29 +1,11 @@
 package http
 
 import (
-	"bufio"
 	"log"
-	"os"
 )
 
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func getStrings(filename string) []string {
-	var lines []string
-	file, err := os.Open(filename)
-	if os.IsNotExist(err) {
-		return nil
-	}
-	check(err)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	check(scanner.Err())
-	return lines
 }
