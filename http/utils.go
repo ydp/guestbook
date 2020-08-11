351994f8b3a86774343dcfc2d10324842570278a
Michael Thaler <michael.thaler@dlr.de>
2020-08-11 08:21:34 +0200

    Added signatures.txt and getStrings function.

--- http/utils.go
+++ http/utils.go
@@ -1,9 +1,29 @@
 package http
 
-import "log"
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getStrings(filename string) []string {
+	var lines []string
+	file, err := os.Open(filename)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
 }
