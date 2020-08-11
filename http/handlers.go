04e1c7e32143641e0265f440d50d6b868a1281bc
Michael Thaler <michael.thaler@dlr.de>
2020-08-11 12:16:51 +0200

    Updated CreateHandler.

--- http/handlers.go
+++ http/handlers.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func ViewHandler(writer http.ResponseWriter, request *http.Request) {
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	signatures := getStrings("signatures.txt")
 	fmt.Printf("%#v\n", signatures)
 	html, err := template.ParseFiles("view.html")
@@ -15,19 +15,19 @@ func ViewHandler(writer http.ResponseWriter, request *http.Request) {
 		SignatureCount: len(signatures),
 		Signatures:     signatures,
 	}
-	err = html.Execute(writer, guestbook)
+	err = html.Execute(w, guestbook)
 	check(err)
 }
 
-func NewHandler(writer http.ResponseWriter, request *http.Request) {
+func NewHandler(w http.ResponseWriter, r *http.Request) {
 	html, err := template.ParseFiles("new.html")
 	check(err)
-	err = html.Execute(writer, nil)
+	err = html.Execute(w, nil)
 	check(err)
 }
 
-func CreateHandler(writer http.ResponseWriter, request *http.Request) {
-	signature := request.FormValue("signature")
-	_, err := writer.Write([]byte(signature))
-	check(err)
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	signature := r.FormValue("signature")
+	fmt.Println(signature)
+	http.Redirect(w, r, "/guestbook", http.StatusFound)
 }
